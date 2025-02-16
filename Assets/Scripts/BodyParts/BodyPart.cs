413c9cf6ff984401d311b82925cdfd48f1887911
Phantom <120600016+Phantomforce260@users.noreply.github.com>
2025-02-16 18:02:42 -0500

    Adding limb scriptable objects

---
+++ Assets/Scripts/BodyParts/BodyPart.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Body Part", menuName = "Body Part")]
+public class BodyPart : ScriptableObject
+{
+    public LimbType Limb;
+    public string Name;
+    
+    [TextArea]
+    public string Description;
+    public Sprite LimbSprite;
+    public int Durability;
+
+    [Range(50, 100), Header("Arms Only")]
+    public int Accuracy;
+    public int Strength;
+
+    [Range(0, 100), Header("Legs Only")]
+    public int Evasion;
+
+    public enum LimbType
+    {
+         Arm, Leg
+    }
+}
