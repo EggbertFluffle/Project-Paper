1f3548726f4785e6f5c9b520e3c3310a704eff5f
Phantom <120600016+Phantomforce260@users.noreply.github.com>
2025-02-18 21:49:12 -0500

    Adding Scene Management code

---
+++ Assets/Scripts/SaveData.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData
+{
+    [Serializable]
+    public class BodyPartRef
+    {
+        public BodyPartRef(BodyPart bodyPart)
+        {
+            Name = bodyPart.Name;
+            LimbType = bodyPart.Limb;
+        }
+
+        public string Name;
+
+        public BodyPart.LimbType LimbType;
+    }
+
+    public List<BodyPartRef> AllBodyParts;
+
+    public List<bool> Checkpoints;
+
+    public void SetCheckpoint(string checkpointName, bool value)
+    {
+        int checkpointValue = checkpointName switch 
+        {
+            "Boss 1" => 0,
+            "Boss 2" => 1,
+            "Boss 3" => 2,
+            "Boss 4" => 3,
+            _ => -1,
+        };
+
+        Checkpoints[checkpointValue] = value;
+    }
+
+    public SaveData()
+    {
+        Checkpoints = new List<bool>
+        {
+            false,
+            false,
+            false,
+            false,
+            false
+        };
+    }
+}
