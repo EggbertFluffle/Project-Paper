e47010c3d4ba83fb43cc20d543e90b5cd9f8f11f
Eggbert Fluffle <hdiambrosio@gmail.com>
2025-02-20 09:33:09 -0500

    fade to black a new main menu background

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,21 @@
 using UnityEngine;
 
-public class MainMenu : MonoBehaviour
-{
-    public void LoadScene(string sceneName) => GameManager.LoadScene(sceneName);
+public class MainMenu : MonoBehaviour {
+    public Animator BackgroundAnimator;
+    public Animator FadeToBlack;
+
+    public void PlayFadeToBlack() {
+        FadeToBlack.Play("FadeToBlack");
+    }
+
+    public void FixedUpdate() {
+        if(Random.Range(0.0f, 1.0f) > 0.999f) {
+            BackgroundAnimator.Play("MainMenu");
+        }
+
+        // Exit the scene once the fade to black is over
+        if(FadeToBlack.GetCurrentAnimatorStateInfo(0).IsName("Finished")) {
+            GameManager.LoadScene("Grave_Robbing");
+        }
+    }
 }
